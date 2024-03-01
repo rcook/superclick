9c8d70c4a68d963a5568e9db88610a34abe7e81f
Richard Cook <rcook@rcook.org>
2024-02-29 22:42:25 -0800

    Reformat

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -19,6 +19,8 @@
 // OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-fn main() -> nih_plug_xtask::Result<()> {
-    nih_plug_xtask::main()
+use nih_plug_xtask::{main as nih_plug_xtask_main, Result};
+
+fn main() -> Result<()> {
+    nih_plug_xtask_main()
 }
